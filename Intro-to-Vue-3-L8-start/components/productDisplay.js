86789c0884b3c079cb0fcb782fa35737adf4a4bf
waiscodes <b.wais@outlook.com>
2020-12-30 16:58:06 -0700

    added premium props and used computed property to assign it a value

--- Intro-to-Vue-3-L8-start/components/productDisplay.js
+++ Intro-to-Vue-3-L8-start/components/productDisplay.js
@@ -1,4 +1,10 @@
 app.component("product-display", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
   template:
     /*html*/
     ` <div class="product-display">
@@ -11,6 +17,7 @@ app.component("product-display", {
             <p v-if="onSale">{{sale}}</p>
             <p v-if="inStock">In Stock</p>
             <p v-else>Out of Stock</p>
+            <p>Shipping: {{shipping}}</p>
             <ul>
               <li v-for="detail in details">{{ detail }}</li>
             </ul>
@@ -76,6 +83,12 @@ app.component("product-display", {
     },
     inStock() {
       return this.variants[this.selectedVariant].quantity;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      }
+      return 2.99;
     },
   },
 });
