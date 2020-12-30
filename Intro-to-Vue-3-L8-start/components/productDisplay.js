745f9192d776c917868a8991bf080b2cf4644298
waiscodes <b.wais@outlook.com>
2020-12-30 15:59:16 -0700

    moved code from main to the component and deleted main's code

--- Intro-to-Vue-3-L8-start/components/productDisplay.js
+++ Intro-to-Vue-3-L8-start/components/productDisplay.js
@@ -33,4 +33,49 @@ app.component("product-display", {
           </div>
         </div>
       </div>`,
+  data() {
+    return {
+      product: "Socks",
+      brand: "Vue Mastery",
+      onSale: true,
+      selectedVariant: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    sale() {
+      return `${this.brand} ${this.product} is ON SALE`;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+  },
 });
