444ead0413739ed90b166aea6872588e666c6aaa
waiscodes <b.wais@outlook.com>
2020-12-30 17:33:31 -0700

    added an emit method to add to cart button and incremented cart

--- Intro-to-Vue-3-L10-start/main.js
+++ Intro-to-Vue-3-L10-start/main.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: 0,
+      premium: true,
+    };
+  },
+  methods: {
+    updateCart() {
+      this.cart++;
     },
-    methods: {}
-})
+  },
+});
